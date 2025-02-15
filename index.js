c95d1f14980fc7f81fc307c280574b24893c131a
Angel <angexcomunicado@gmail.com>
2025-02-15 15:09:51 -0700

    Funcionalidad de cambiar imagen con flechas

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const imagenes = document.querySelectorAll('.imagen');
 const modal = document.getElementById('modal1');
 const close = document.getElementById('close');
 const modalImage = document.getElementById('imageModal');
+const imagesArray = Array.from(imagenes);
+let currentIndex = 0;
 
-imagenes.forEach((image) => {
+imagesArray.forEach((image, index) => {
     image.addEventListener('click', () => {
         modal.style.display = 'block';
         modalImage.src = image.src;
+        currentIndex = index;
     })
 }) 
 
@@ -24,5 +27,11 @@ modal.addEventListener('click', (event) => {
 document.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
         modal.style.display = 'none';
+    } else if (event.key === 'ArrowRight') {
+        currentIndex = (currentIndex + 1) % imagesArray.length;
+        modalImage.src = imagesArray[currentIndex].src;
+    } else if (event.key === 'ArrowLeft') {
+        currentIndex = (currentIndex - 1 + imagesArray.length) % imagesArray.length;
+        modalImage.src = imagesArray[currentIndex].src;
     }
 })
